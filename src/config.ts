4530fc4583a48a5c91d8f16626fbd3d7ecbe8423
Ravi Pangali <lilathapamagar453@gmail.com>
2026-02-26 00:30:27 +0545

    CHanges

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@
 //   (import.meta.env.VITE_HOME_PAGE as string)?.trim()?.toLowerCase() === "second"
 //     ? "second"
 //     : "first";
-export const HOME_PAGE_VARIANT = "second";
-// export const HOME_PAGE_VARIANT = "first";
+// export const HOME_PAGE_VARIANT = "second";
+export const HOME_PAGE_VARIANT = "first";
 
 export type HomePageVariant = "first" | "second";
