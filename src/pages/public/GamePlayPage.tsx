e3f71d1b85c505f43f180ab20ada02a97d11f328
Ravi Pangali <lilathapamagar453@gmail.com>
2026-03-07 12:48:22 +0545

    changes

--- src/pages/public/GamePlayPage.tsx
+++ src/pages/public/GamePlayPage.tsx
@@ -124,7 +124,7 @@ export default function GamePlayPage() {
         <iframe
           title="Game"
           src={launchUrl}
-          className="absolute inset-0 w-full h-full min-w-0 min-h-0 border-0"
+          className="absolute inset-0 min-w-0 min-h-0 border-0 w-[100vw] h-[100vh]"
           allow="fullscreen; payment; autoplay"
           allowFullScreen
         />
