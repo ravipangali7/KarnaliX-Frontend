8b7e7b4d1febf6fa1c8cb9016caca332bd8a2523
targetdubai2026 <targetdubai2026@gmail.com>
2026-03-27 17:56:54 +0545

    CHNages

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -10,9 +10,9 @@
 // export const BASE_URL = "https://bhtadmin.luckyuser365.com/api";
 
 // export const BASE_URL = "https://admin.luckyuser365.com/api";
-export const BASE_URL = "https://admin.lucky365ind.com/api";
+// export const BASE_URL = "https://admin.lucky365ind.com/api";
 // export const BASE_URL = "https://admin.lucky365bhd.com/api";
-// export const BASE_URL = "https://admin.lucky365bht.com/api";
+export const BASE_URL = "https://admin.lucky365bht.com/api";
 
 function getToken(): string | null {
   return localStorage.getItem('token');
